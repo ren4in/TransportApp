26fa05927fb0a90ab648b9ec090f08b2db05a925
ren4in <ren4in@gmail.com>
2026-02-13 10:16:22 +0300

    Добавил класс водителя

---
+++ TransportApp/Driver.cs
@@ -0,0 +1,31 @@
+﻿
+using Microsoft.VisualBasic;
+
+namespace TransportApp
+    {
+        public class Driver
+        {
+            public int Id {  get; set; }
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public DateTime BirthDate { get; set; }
+            public string LicenseNumber { get; set; }
+            public Driver(string firstName, string lastName, DateTime birthDate, string licenseNumber)
+            {
+                FirstName = firstName;
+                LastName = lastName;
+                BirthDate = birthDate;
+                LicenseNumber = licenseNumber;
+            }
+
+        public int GetAge()
+        {
+            DateTime now = DateTime.Today;
+            int age = now.Year - BirthDate.Year;
+
+            if (BirthDate > now.AddYears(-age))
+                age--;
+            return age;
+        }
+        }
+    }
--- TransportApp/Program.cs
+++ TransportApp/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace TransportApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            Driver[] drivers =
+            {
+                new Driver("Иван", "Иванов", new DateTime(2000, 2, 13), "3576453492"),
+                new Driver("Петр", "Петров", new DateTime(2000, 3, 26), "9876543210")
+            };
+
+            Console.WriteLine($"Возраст первого водителя: {drivers[0].GetAge()}");
+            Console.WriteLine($"Возраст второго водителя: {drivers[1].GetAge()}");
+
+        }
+    }
+}
